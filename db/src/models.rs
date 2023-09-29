e6b30de8b73498df4d8134aef2e17c0a9e682188
jthomas981 <john.thomas.mouse@gmail.com>
2023-09-28 20:33:52 -0400

    Added Friend and FriendStatus models

--- db/src/models.rs
+++ db/src/models.rs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 use mongodb::bson::oid::ObjectId;
 use serde::{Deserialize, Serialize};
 
@@ -14,3 +15,22 @@ enum FriendStatus {
     InitialesRequested,
     Accepted,
 }
+=======
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+struct TempNumber {
+    phone_number: String,
+    verification: VerificationStatus,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+enum VerificationStatus {
+    NotVerified {
+        code: String,
+        expiration: DateTime<Utc>,
+    },
+    Verified,
+}
+>>>>>>> c3a856eade390b33176467d633352809b197bcb3
