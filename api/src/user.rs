2be765de0c00e77a5a36bff76dfae2ec3d71fbbe
Thomas Lane <70451454+thomas-lane@users.noreply.github.com>
2023-10-13 00:32:42 +0000

    Added basic user search skeleton

--- api/src/user.rs
+++ api/src/user.rs
@@ -1,8 +1,10 @@
-use actix_web::{HttpResponse, Responder, route};
+use actix_web::{HttpResponse, Responder, route, web};
 use build_time::build_time_utc;
+use serde::Deserialize;
 
 pub fn config(cfg: &mut actix_web::web::ServiceConfig) {
     cfg.service(test);
+    cfg.service(search);
 }
 
 #[route("/test", method = "GET", method = "HEAD")]
@@ -10,5 +12,18 @@ async fn test() -> impl Responder {
     HttpResponse::Ok().body(format!(
         "Mixtape API Server TWO\nBuild Timestamp {}",
         build_time_utc!()
+    ))
+}
+
+#[derive(Deserialize)]
+struct SearchInfo {
+    query: String,
+}
+
+#[route("/search", method = "GET")]
+async fn search(search_info: web::Json<SearchInfo>) -> impl Responder {
+    HttpResponse::Ok().body(format!(
+        "Success!\n/u/search with a query of: \"{}\"",
+        search_info.query,
     ))
 }
