a06a2b41185d3cd74f32270ff19e34b86ad17b09
cole-p-murphy <murph607@purdue.edu>
2023-10-17 18:36:29 -0400

    block user api endpoint

--- api/src/safety.rs
+++ api/src/safety.rs
@@ -2,12 +2,14 @@ use actix_web::{HttpResponse, Responder, route};
 use build_time::build_time_utc;
 
 pub fn config(cfg: &mut actix_web::web::ServiceConfig) {
-    cfg.service(block);
+    cfg.service(test);
 }
 
-#[route("/test", method = "GET")]
-async fn test() -> impl Responder {
+#[route("/u/{id/block}", method = "PUT", method = "HEAD")]
+async fn block() -> impl Responder {
     HttpResponse::Ok().body(format!(
+        {id},
+        
         "Mixtape API Server TWO\nBuild Timestamp {}",
         build_time_utc!()
     ))
