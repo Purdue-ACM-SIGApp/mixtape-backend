7dec372b5cad0b8d90072cda5e6c40ecfefc04e6
cole-p-murphy <murph607@purdue.edu>
2023-10-17 18:58:26 -0400

    api endpoint for delete_device

--- api/src/user.rs
+++ api/src/user.rs
@@ -1,8 +1,8 @@
-use actix_web::{HttpResponse, Responder, route};
+use actix_web::{HttpResponse, Responder, route, web};
 use build_time::build_time_utc;
 
 pub fn config(cfg: &mut actix_web::web::ServiceConfig) {
-    cfg.service(test);
+    cfg.service(test).service(delete_device);
 }
 
 #[route("/test", method = "GET")]
@@ -10,5 +10,15 @@ async fn test() -> impl Responder {
     HttpResponse::Ok().body(format!(
         "Mixtape API Server TWO\nBuild Timestamp {}",
         build_time_utc!()
+    ))
+}
+
+#[route("/u/me/device/{id}", method = "DELETE")]
+async fn delete_device(path: web::Path<u32>) -> impl Responder {
+    let id = path.into_inner();
+
+    HttpResponse::Ok().body(format!(
+        "Deleted device with id {}",
+        id,
     ))
 }
