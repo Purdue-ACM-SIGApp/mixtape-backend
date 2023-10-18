42bfa189ecfe9d3131918ba805054792970748b6
cole-p-murphy <murph607@purdue.edu>
2023-10-17 20:04:05 -0400

    api endpoint for unblocking user

--- api/src/safety.rs
+++ api/src/safety.rs
@@ -1,7 +1,9 @@
 use actix_web::{HttpResponse, Responder, route, web};
 
 pub fn config(cfg: &mut actix_web::web::ServiceConfig) {
-    cfg.service(block);
+    cfg
+    .service(block)
+    .service(unblock);
 }
 
 #[route("/u/{id}/block", method = "PUT")]
@@ -10,6 +12,16 @@ async fn block(path: web::Path<u32>) -> impl Responder {
 
     HttpResponse::Ok().body(format!(
         "Blocked user with id {}",
+        id,
+    ))
+}
+
+#[route("/u/{id}/block", method = "DELETE")]
+async fn unblock(path: web::Path<u32>) -> impl Responder {
+    let id = path.into_inner();
+
+    HttpResponse::Ok().body(format!(
+        "Unblocked user with id {}",
         id,
     ))
 }
