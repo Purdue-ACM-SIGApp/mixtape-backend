3021bd37fc87456800da12fba75be1c7a4c02005
jthomas981 <john.thomas.mouse@gmail.com>
2023-10-24 19:51:29 -0400

    Added update_playlist and delete_playlist endpoints

--- api/src/playlist.rs
+++ api/src/playlist.rs
@@ -4,7 +4,9 @@ pub fn config(cfg: &mut actix_web::web::ServiceConfig) {
     cfg
         .service(create_playlist)
         .service(get_playlists)
-        .service(get_playlist);
+        .service(get_playlist)
+        .service(update_playlist)
+        .service(delete_playlist);
 }
 
 #[route("/p/new", method = "POST")]
@@ -27,5 +29,23 @@ async fn get_playlist(path: web::Path<u32>) -> impl Responder {
 
     HttpResponse::Ok().body(format!(
         "Found playlist with ID: {}", id
+    ))
+}
+
+#[route("/p/{id}", method = "PUT")]
+async fn update_playlist(path: web::Path<u32>) -> impl Responder {
+    let id = path.into_inner();
+
+    HttpResponse::Ok().body(format!(
+        "Updated playlist with ID: {}", id
+    ))
+}
+
+#[route("/p/{id}", method = "DELETE")]
+async fn delete_playlist(path: web::Path<u32>) -> impl Responder {
+    let id = path.into_inner();
+
+    HttpResponse::Ok().body(format!(
+        "Deleted playlist with ID: {}", id
     ))
 }
