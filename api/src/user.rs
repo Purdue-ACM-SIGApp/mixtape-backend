75081084ad6c2c03f5f3ab10dada1fc3d4d748d9
dhsiehcode <89666559+dhsiehcode@users.noreply.github.com>
2023-10-24 20:03:05 -0400

    attempted to write GET /u/me

--- api/src/user.rs
+++ api/src/user.rs
@@ -1,8 +1,12 @@
 use actix_web::{HttpResponse, Responder, route};
 use build_time::build_time_utc;
+use serde::Deserialize;
 
 pub fn config(cfg: &mut actix_web::web::ServiceConfig) {
-    cfg.service(test);
+    cfg
+    .service(test)
+    .service(me)
+    .service(search);
 }
 
 #[route("/test", method = "GET", method = "HEAD")]
@@ -11,4 +15,12 @@ async fn test() -> impl Responder {
         "Mixtape API Server TWO\nBuild Timestamp {}",
         build_time_utc!()
     ))
+}
+
+
+#[route("/me", method = "GET")]
+async fn me() -> impl Responder {
+    HttpResponse::Ok().body(format!{
+        "/u/me"
+    })
 }
