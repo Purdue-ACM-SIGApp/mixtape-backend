78be66124a65084c77c4e4ce625d1dea30a1b990
colin wu <colinwu0403@gmail.com>
2023-10-26 20:18:40 -0400

    Added basic functionality for get_friends and share

--- api/src/friend.rs
+++ api/src/friend.rs
@@ -22,8 +22,8 @@ struct FriendRet {
     status: FriendStatus
 }
 
-#[route("/f", method = "GET")]
-async fn get_friends(db: &Database, user:ID) -> anyhow::Result<Vec<FriendRet>> {
+mod internal {
+  async fn get_friends(db: &Database, user:ID) -> anyhow::Result<Vec<FriendRet>> {
 
     // let client = Client::with_uri_str("mongodb://localhost:27017/your_database").await?;
     // let db = client.database("your_database");
@@ -33,6 +33,60 @@ async fn get_friends(db: &Database, user:ID) -> anyhow::Result<Vec<FriendRet>> {
     // let collection: Collection<FriendRet> = db.collection(&collection_name);
 
     // let user_id: String = user.into(); // Assuming user is convertible to String
+
+    let filter = doc! {"_id": user};
+
+    let cursor = collection.find(filter, None).await?;
+
+    let mut friends: Vec<FriendRet> = vec![];
+    for result in cursor {
+        let friend: FriendRet = result?;
+        friends.push(friend);
+    }
+
+    Ok(friends);
+  }
+
+  async fn delete_friend(path: web::Path<u32>) -> impl Responder {
+    let id = path.into_inner();
+
+    HttpResponse::Ok().body(format!(
+        "Deleted friend with id {}", id
+    ))
+  }
+
+  async fn find_friend(request: web::Json<FindFriendRequest>) -> impl Responder {
+    let phone_numbers: Vec<String> = request.phone_numbers.clone();
+
+    HttpResponse::Ok().body(format!(
+        "Found friends based upon these phone numbers: {:?}", phone_numbers
+    ))
+  }
+
+  async fn share(db: &Database, user:ID, phone:&impl Phone, numbers: Vec<String>) -> impl Responder {
+
+    let phone_numbers: Vec<String> = numbers.clone();
+
+    // Shares Mixtape with friends via SMS (currently it's just a HttpResponse,
+    // but there will be functionality to send text messages)
+
+    let message = "🎶 Introducing Mixtape: The Ultimate Playlist Collaboration App! 🎶\n
+        Hey there, music lovers! 🎧\n
+        I've just stumbled upon something incredible called Mixtape, and it's a game-changer for music enthusiasts like us:\n
+        🤝 Collaborate with friends on the perfect playlist.\n
+        🎵 Syncs seamlessly with Spotify and Apple Music.\n
+        💬 Share your amazing playlists effortlessly.\n
+        Get started today! Download Mixtape now:\n
+        App Store: [Insert App Store Link]\n
+        Google Play: [Insert Google Play Link]\n";
+    
+    HttpResponse::Ok().body(message)
+
+  }
+}
+
+#[route("/f", method = "GET")]
+async fn get_friends(data:Data<State>, user:AuthedUser) -> anyhow::Result<Vec<FriendRet>> {
 
     let filter = doc! {"_id": user};
 
@@ -66,7 +120,7 @@ async fn find_friend(request: web::Json<FindFriendRequest>) -> impl Responder {
 }
 
 #[route("/f/share", method = "POST")]
-async fn share(db: &Database, user:ID, phone:&impl Phone, numbers: Vec<String>) -> impl Responder {
+async fn share(data:Data<State>, user:AuthedUser) -> impl Responder {
 
     let phone_numbers: Vec<String> = numbers.clone();
 
@@ -75,15 +129,13 @@ async fn share(db: &Database, user:ID, phone:&impl Phone, numbers: Vec<String>) 
 
     let message = "🎶 Introducing Mixtape: The Ultimate Playlist Collaboration App! 🎶\n
         Hey there, music lovers! 🎧\n
-        I've just stumbled upon something incredible that's about to revolutionize the way we share and discover music.\n
-        It's called Mixtape, and it's a game-changer for music enthusiasts like us:\n
+        I've just stumbled upon something incredible called Mixtape, and it's a game-changer for music enthusiasts like us:\n
         🤝 Collaborate with friends on the perfect playlist.\n
         🎵 Syncs seamlessly with Spotify and Apple Music.\n
         💬 Share your amazing playlists effortlessly.\n
         Get started today! Download Mixtape now:\n
         App Store: [Insert App Store Link]\n
-        Google Play: [Insert Google Play Link]\n
-        Let's create musical magic together! 🎉🎵✨ #MixtapeApp";
+        Google Play: [Insert Google Play Link]\n";
     
     HttpResponse::Ok().body(message)
 
